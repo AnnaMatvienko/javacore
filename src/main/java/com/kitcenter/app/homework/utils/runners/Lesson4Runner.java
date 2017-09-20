2f70e2229d5698e3f991e3d284af8b05a18f952d
Anna Matviienko <anna.matvienko08@gmail.com>
2017-09-20 15:45:09 +0300

    Add primitive convertor logic class and runner

--- src/main/java/com/kitcenter/app/homework/utils/runners/Lesson4Runner.java
+++ src/main/java/com/kitcenter/app/homework/utils/runners/Lesson4Runner.java
@@ -8,7 +8,9 @@ public class Lesson4Runner {
     public static void main(String[] args) {
         PrimitiveConvertor pconvertor = new PrimitiveConvertor();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter the float variable value:");
         pconvertor.floatToChar(scanner.nextFloat());
+        System.out.println("Please, enter the integer variable value:");
         pconvertor.intToChar(scanner.nextInt());
         pconvertor.charToInt();
     }
