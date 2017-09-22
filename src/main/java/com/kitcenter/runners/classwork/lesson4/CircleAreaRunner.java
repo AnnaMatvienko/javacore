51a8301e9d707d5ffc3cea521a1b31cb9d3d3c17
Anna Matviienko <anna.matvienko08@gmail.com>
2017-09-22 11:35:20 +0300

    Synchronize master branch

--- src/main/java/com/kitcenter/runners/classwork/lesson4/CircleAreaRunner.java
+++ src/main/java/com/kitcenter/runners/classwork/lesson4/CircleAreaRunner.java
@@ -2,6 +2,8 @@ package com.kitcenter.runners.classwork.lesson4;
 
 import com.kitcenter.app.classwork.lesson4.CircleArea;
 
+import java.util.Scanner;
+
 public class CircleAreaRunner {
     public static void main(String[] args) {
         CircleArea area = new CircleArea();
